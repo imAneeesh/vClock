cb87311a3a0b6bc3af7b9cb30bc3c5326edd7a88
sathwik-14 <kywagle@gmail.com>
2023-07-06 15:21:14 +0530

    dark-mode

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const menuIcon = document.getElementById("menuIcon");
 const element = document.body;
 const modeIcon = document.getElementById("modeIcon");
-
+const topbar=document.getElementById("top-bar");
+const menubar=document.getElementById("menu");
 function toggleDarkMode() {
     modeIcon.classList.toggle("fa-moon");
-
+    topbar.classList.toggle("dark-mode");
+    menubar.classList.toggle("dark-mode");
     element.classList.toggle("dark-mode");
 }
 
